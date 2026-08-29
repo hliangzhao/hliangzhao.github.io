9d9a01eac9638c7aab69aa37faeb4edddd7d91bf
Hailiang Zhao <hliangzhao@192.168.2.87>
2026-08-30 00:16:45 +0800

    Add technical research note pages

---
+++ js/mathjax-config.js
@@ -0,0 +1,10 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [['\\(', '\\)']],
+    displayMath: [['\\[', '\\]']],
+    processEscapes: true
+  },
+  options: {
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
+  }
+};
